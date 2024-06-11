3d9c906611d69f60aab785520728d6ed943494fd
WalterCorsini <corsini.walter@gmail.com>
2024-06-11 14:48:52 +0200

    add attribute version mysql

--- database/migrations/2024_06_11_120939_trains.php
+++ database/migrations/2024_06_11_120939_trains.php
@@ -8,20 +8,32 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *  CREATE TABLE 'trains' (
+     *      'code_train' Unsigned BIGINT NOT NULL,
+     *      'name' VARCHAR(255) NOT NULL,
+     *      'number_of_carriages' UNSIGNED TINYINT NULL,
+     *      'departure_station' VARCHAR(255) NOT NULL,
+     *      'arrival_station' VARCHAR(255) NOT NULL,
+     *      'departure_time TIME,
+     *      'arrival_time' TIME,
+     *      'in_time' UNSIGNED TINIYNT NOT NULL DEFAULT(1)
+     *      'delete' UNSIGNED TINIYNT NOT NULL DEFAULT(0)
+     *       PRIMARY KEY ('train_code')
+     *  )
      */
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table){
             //  campi da inserire
-            $table->unsignedBigInteger('train_code')->unique; // treni hanno codice da 12 cifre numeriche
+            $table->unsignedBigInteger('train_code')->unique; // treni hanno codice da 12 cifre numeriche univoco verrà usato come id.
             $table->string('name');  // possono avere lo stesso nome (freccarossa,frecciargento.... ma no lo stesso train_code che gli distingue)
-            $table->unsignedTinyInteger('number of carriages')->nullable();
+            $table->unsignedTinyInteger('number_of_carriages')->nullable();
             $table->string('departure_station');
             $table->string('arrival_station');
             $table->time('departure_time');
             $table->time('arrival_time');
-            $table->boolean('in_time')->nullable();
-            $table->boolean('in_time')->nullable();
+            $table->boolean('in_time')->default(true);
+            $table->boolean('delete')->default(false);
         });
     }
 
